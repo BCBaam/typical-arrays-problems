9f031391ce887e7a15fd6a677233ae31c085a4b5
Anton Seniuta <senita.anton@gmail.com>
2021-03-15 02:29:50 +0800

    init: add task solution

--- src/index.js
+++ src/index.js
@@ -1,29 +1,16 @@
 exports.min = function min (array) {
     if (arguments.length>0){
         if (array.length>0) {
-            let min = array[0];
-            for(let i = 0; i < array.length; i++){
-                if(min > array[i]){
-                    min = array[i];
-                }
-            }
-            return min;
+            return Math.min.apply(null,array);
         }
     }
     return 0;
-
 }
 
 exports.max = function max (array) {
     if (arguments.length>0){
         if (array.length>0) {
-            let max = array[0];
-            for(let i = 0; i < array.length; i++){
-                if(max < array[i]){
-                    max = array[i];
-                }
-            }
-            return max;
+            return Math.max.apply(null,array);
         }
     }
     return 0;
@@ -40,5 +27,4 @@ exports.avg = function avg (array) {
         }
     }
     return 0;
-
 }
